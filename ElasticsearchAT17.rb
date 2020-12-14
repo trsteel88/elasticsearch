279adf44838ab1a440c37c86efef8b6b1c6fa60a
Trent Steel <tsteel@vivogroup.com.au>
2020-12-14 20:21:25 +1000

    Update dependancy

--- ElasticsearchAT17.rb
+++ ElasticsearchAT17.rb
@@ -9,7 +9,7 @@ class Elasticsearchat17 < Formula
 
   keg_only :versioned_formula
 
-  depends_on :java => "1.7+"
+  depends_on :java => "openjdk@8"
 
   def cluster_name
     "elasticsearch_#{ENV["USER"]}"
